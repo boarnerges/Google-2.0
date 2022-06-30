c3a27e4458b97f26f232843b695599b8afe0171a
olusegun Owolabi <olujawo1996@gmail.com>
2022-06-30 14:36:54 +0100

    Header/Search/Response/HeaderOption/

---
+++ gooogle-2.0/Keys.js
@@ -0,0 +1,7 @@
+// API keys - https://developers.google.com/custom-search/v1/using_rest 
+
+export const API_KEY = 'AIzaSyA6rFjDNh-gsDQXcBNeT7NdZee-t0YzMWk'
+
+//Const Key - https://cse.google.com/cse/create/new
+
+export const CONTEXT_KEY = ' 9b2779deaa39eb926'
--- gooogle-2.0/pages/index.js
+++ gooogle-2.0/pages/index.js
@@ -3,9 +3,25 @@ import Avatar from '../components/Avatar';
 import {MicrophoneIcon, SearchIcon, ViewGridIcon} from "@heroicons/react/solid"
 import Image from "next/image"
 import Footer from '../components/Footer';
+import { useRef } from 'react';
+import { useRouter } from 'next/router';
 
 
 export default function Home() {
+  
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term} `);
+
+  };
+
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <Head>
@@ -47,13 +63,13 @@ export default function Home() {
         />
         <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl' >
           <SearchIcon className='h-5 mr-3 text-gray-500'/>
-          <input type= "text" className='flex-grow focus:outline-none'/>
+          <input ref = {searchInputRef} type= "text" className='flex-grow focus:outline-none'/>
           <MicrophoneIcon className='h-5'/>
         </div>
 
         <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'>
-          <botton className = 'btn'>Google Search</botton>
-          <botton className = 'btn'>I'm feeling Lucky</botton>
+          <botton  onClick={search} className = 'btn'>Google Search</botton>
+          <botton onClick={search} className = 'btn'>I'm feeling Lucky</botton>
         </div>
       </form>
 
